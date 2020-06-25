eed46d96fa11ca724f5e4ff9e781d2e151fb0280
Bridgette <bridgette_lafaye@berkeley.edu>
2020-06-24 20:21:02 -0700

    Redirected home page to sample home_page.html; Added example tutorial files (_lulu)

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 from flask import Flask, render_template, request, redirect
+import pandas as pd
+import bokeh
+import requests
+import simplejson
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-  return render_template('index.html')
+def home_page():
+  return render_template('home_page.html')
 
-@app.route('/about')
-def about():
-  return render_template('about.html')
+# @app.route('/about')
+# def about():
+#   return render_template('about.html')
 
-@app.route('/hello_page_test')
-def hello_world():
-    return 'Hello world'
+# @app.route('/hello_page_test')
+# def hello_world():
+#     return 'Hello world'
+
+@app.route('/user_input')
+def user_input():
+    return render_template('userinfo_lulu.html')
 
 if __name__ == '__main__':
   app.run(port=33507)
